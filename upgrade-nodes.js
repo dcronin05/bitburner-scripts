5b4bf836d27552c1b3baaede5b2f4e5b7cc0a8e7
Daniel Cronin <daniel@dcron.in>
2024-12-12 22:09:43 -0600

    improve upgrade-nodes script

--- upgrade-nodes.js
+++ upgrade-nodes.js
@@ -4,19 +4,20 @@ export async function main(ns) {
         var numNodes = ns.hacknet.numNodes();
         for (var i = numNodes - 1; i > -1; i--) {
             var name = ns.hacknet.getNodeStats(i);
-
             if (ns.hacknet.purchaseNode() !== -1 ) {
                 ns.tprint(`Purchased new node!!`);
             }
-
-            ns.print("node - ", name.name, "Level - ", name.level);
+            
             while(ns.hacknet.upgradeLevel(i)) {
+                name = ns.hacknet.getNodeStats(i);
                 ns.tprint(`Node ${i} upgraded to level ${name.level}.`);
             }
             while(ns.hacknet.upgradeCore(i)) {
+                name = ns.hacknet.getNodeStats(i);
                 ns.tprint(`Node ${i} upgraded to ${name.cores} cores.`)
             }
             while(ns.hacknet.upgradeRam(i)) {
+                name = ns.hacknet.getNodeStats(i);
                 ns.tprint(`Node ${i} RAM upgraded to ${name.ram}GB.`)
             }
         }
